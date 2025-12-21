799d29317eee6632ed807c97f4d55fecc049b23b
Kenley Cheung <ken@lordkenthegreat.com>
2025-12-21 17:46:41 -0500

    Add TwitchTicketsDao

---
+++ lib/src/main/java/org/seattleoba/ticketing/jdbi/TwitchTicketsDao.java
@@ -0,0 +1,18 @@
+package org.seattleoba.ticketing.jdbi;
+
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+public interface TwitchTicketsDao {
+    @SqlQuery("SELECT twitch_id FROM twitch_accounts_bevy_tickets WHERE bevy_ticket_id=:ticket_id")
+    Integer getUserForTicket(@Bind("ticket_id") int ticketNumber);
+
+    @SqlQuery("SELECT bevy_ticket_id FROM twitch_accounts_bevy_tickets WHERE twitch_id=:user_id")
+    List<Integer> getTicketsForUser(@Bind("user_id") int userId);
+
+    @SqlUpdate("INSERT INTO twitch_accounts_bevy_tickets (twitch_id, bevy_ticket_id) VALUES (:user_id, :ticket_id)")
+    void insert(@Bind("user_id") int userId, @Bind("ticket_id") int ticketNumber);
+}
---
+++ lib/src/test/java/org/seattleoba/jdbi/TwitchTicketsDaoTest.java
@@ -0,0 +1,36 @@
+package org.seattleoba.jdbi;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.seattleoba.ticketing.jdbi.TwitchTicketsDao;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TwitchTicketsDaoTest {
+    private static final String JDBI_URL = "jdbc:sqlite:/tmp/soba-test/sample.db";
+
+    private Jdbi jdbi;
+
+    @BeforeEach
+    public void setup() {
+        jdbi = Jdbi.create(JDBI_URL);
+        jdbi.installPlugin(new SqlObjectPlugin());
+    }
+
+    @Test
+    public void insertsRowIntoTable() {
+        final Integer userId = Math.toIntExact(Instant.now().getEpochSecond() % 999000000);
+        final Integer ticketId = Math.toIntExact(Instant.now().toEpochMilli() & 100000);
+        final TwitchTicketsDao dao = jdbi.onDemand(TwitchTicketsDao.class);
+
+        assertDoesNotThrow(() -> dao.insert(userId, ticketId));
+
+        assertEquals(userId, dao.getUserForTicket(ticketId));
+        assertFalse(dao.getTicketsForUser(userId).isEmpty());
+        assertEquals(ticketId, dao.getTicketsForUser(userId).getFirst());
+    }
+}
