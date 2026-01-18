43cba02ba6ce8328b6100ebd8037d2abf4fa0f05
Kenley Cheung <ken@lordkenthegreat.com>
2026-01-17 21:04:24 -0800

    Add EventRegistration

--- lib/src/main/java/org/seattleoba/data/dynamodb/bean/EventRegistration.java
+++ lib/src/main/java/org/seattleoba/data/dynamodb/bean/EventRegistration.java
@@ -1,7 +1,43 @@
 package org.seattleoba.data.dynamodb.bean;
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
 
 @DynamoDbBean
 public class EventRegistration {
+    private Integer id;
+    private Integer eventId;
+    private Integer twitchId;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    @DynamoDbSecondaryPartitionKey(indexNames = "EventId")
+    @DynamoDbAttribute(value = "bevy_event_id")
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(final Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    @DynamoDbSecondaryPartitionKey(indexNames = "UserId")
+    @DynamoDbAttribute(value = "twitch_id")
+    public Integer getTwitchId() {
+        return twitchId;
+    }
+
+    public void setTwitchId(final Integer twitchId) {
+        this.twitchId = twitchId;
+    }
 }
